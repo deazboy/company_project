6d407c438bc2aa70a89b5f3627aa4bbb924e33bc
deazboy <s.voshchuk2015@yandex.ru>
2024-04-04 14:28:35 +0300

    исправление тестов на вычитание и конвертацию

--- ConsoleApp1/ConverterTest/ConverterTest.cs
+++ ConsoleApp1/ConverterTest/ConverterTest.cs
@@ -14,21 +14,15 @@ namespace ConverterTest
     public class ConverterTest
     {
         [Theory]
-        [InlineData(55.55, "55.55")]
+        [InlineData(55.55, "55.55"),
+         InlineData(11.11, "11,11")]
         public void Convert(double res, string vinput)
         {
             var loggerMock = new Mock<ILogger<Converter>>();
             var _converter = new Converter(loggerMock.Object);
             Assert.Equal(res, _converter.ConvertData(vinput));
-        }
-        [Theory]
-        [InlineData(11.11, "11,11")]
-        public void Conv(double res, string vinput)
-        {
-            var loggerMock = new Mock<ILogger<Converter>>();
-            var _converter = new Converter(loggerMock.Object);
             Assert.Equal(res, _converter.ConvertData(vinput));
-        }
+        }       
         [Theory]
         [InlineData(28.5, "28m5")]
         public void CalсAdd(double res, string vinput)
